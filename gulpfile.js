b6bad3f56b35616986394da62fe1ce39ec49a00b
Jonathon Choo <jonno.choo@gmail.com>
2015-05-19 22:45:14 +1000

    Initial version with HTML only

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var notify = require("gulp-notify");
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+
+gulp.task('default', ['nodemon', 'sass-watch'])
+
+gulp.task('sass-watch', function() {
+  gulp.watch('scss/**/*.scss', ['sass']);
+});
+
+gulp.task('sass', function() {
+  return gulp.src('scss/app.scss')
+    .pipe(sass({ 
+      errLogToConsole: true
+    }))
+    .pipe(gulp.dest('css/'));
+//    .pipe(notify("SASS recompiled"));
+});
