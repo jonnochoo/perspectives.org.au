8be899340cf93612fd75fc7bea928350904a02d2
Jonathon Choo <jonno.choo@gmail.com>
2018-09-06 20:57:56 +1000

    Added captcha

--- routes/contact/index.js
+++ routes/contact/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var email = require('../../lib/email');
+var request = require('request');
+var config = require('../../config');
 
 var router = express.Router();
 router.get('/', getContact);
@@ -14,12 +16,21 @@ function getContact(req, res) {
     });
 }
 
-function postContact(req, res) {    
+function postContact(req, res) {
     var subject = 'Perspectives Enquiry: ' + req.body.name;
     var body = req.body.message;
     var from = req.body.from;
 
-    email.send(subject, body, from, function(err) {
-        res.redirect('confirmation');
+    var secretKey = config.express.recaptchaSecret;
+    var uri = 'https://www.google.com/recaptcha/api/siteverify?secret=' + secretKey + '&response=' + req.body['g-recaptcha-response'];
+    request.post({ uri: uri, method: 'POST' }, function (req1, res1, body1) {
+        var result = JSON.parse(body1);
+        if (result.success) {
+            email.send(subject, body, from, function (err) {
+                res.redirect('confirmation');
+            });
+        } else {
+            res.send('400');
+        }
     });
 }
